1aed68c9bc5d4417cf4731a8f4196bca2a11ba48
Zen-333 <zenhamam56@gmail.com>
2026-02-07 16:32:34 +0000

    adding comments

--- 03_postgresql/db.js
+++ 03_postgresql/db.js
@@ -5,4 +5,7 @@ import {neon} from '@neondatabase/serverless';
 if(!process.env.DATABASE_URL) throw new Error('DATABASE_URL is required');
 
 const sql = neon(process.env.DATABASE_URL);
+
+// Creates a query function using your Neon connection string. This sql function can execute raw SQL queries.
+// When you call neon(process.env.DATABASE_URL), it returns a function that you can use to execute SQL queries.
 export const db = drizzle(sql);
